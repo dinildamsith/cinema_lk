8c5975d19fd821b51427a58a15c9a94f02abd6a1
dinildamsith <dinildamsith52@gmail.com>
2025-05-08 22:54:40 +0530

    setup project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Typography} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <Typography variant="h4" gutterBottom>
+              MUI is working!
+          </Typography>
+          <Button variant="contained" color="primary">
+              Click Me
+          </Button>
+      </div>
   );
 }
 
